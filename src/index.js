9378d197618fbfe9dc997ed62fa65b8054623322
Javier Duarte <javierandresduarte@gmail.com>
2019-10-09 20:12:48 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -41,9 +41,10 @@ connectDb().then(async () => {
     createUsersWithMessages();
   }
 
-  app.listen(process.env.PORT || 5000, () =>
-    console.log(`App Escuchando en ${process.env.PORT}!`),
-  );
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
 });
 
 
